2d8a843698dc25447eaa8b6f92cfe00182364474
santooooooo <santo.shunsuke@gmail.com>
2023-03-13 18:20:40 +0900

    -0.72, 51.051

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function main() {
   //arjs.add(box, 35.66322707826773, 139.38779162906647);
 
   arjs.fakeGps(139.38845690593965, 35.65749057639006);
-  arjs.startGps();
+  //arjs.startGps();
 
   requestAnimationFrame(render);
 
